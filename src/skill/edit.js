bb5406c66faed2ae2579f8ba2ac3b94b6163116f
Dennis Claassen <declaassen@gmail.com>
2025-06-18 23:20:31 +0200

    Update edit.js

--- src/skill/edit.js
+++ src/skill/edit.js
@@ -3,6 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
+import { TextControl } from '@wordpress/components';
 
 /**
  * Internal dependencies.
