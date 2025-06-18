912d4e69b0ea95c9571a3332796399ca45e245c9
Dennis Claassen <declaassen@gmail.com>
2025-06-18 07:22:12 +0200

    Create save.js

---
+++ src/skill/save.js
@@ -0,0 +1,19 @@
+import { useBlockProps } from ‘@wordpress/block-editor’;
+
+export default function save({ attributes }) {
+const { name, description } = attributes;
+
+return (
+	<div {...useBlockProps.save()}>
+		<div className="skills-block">
+						<div className="skill-item">
+							<span className="skill-name">{name}</span>
+
+							{description && (
+								<span className="skill-description">{skill.description}</span>
+							)}
+						</div>
+		</div>
+	</div>
+);
+}
